eed9325d32c6f818ee569550756efe696ed5a115
iasos2 <ykkminji@gmail.com>
2024-06-26 22:40:38 +0900

    Create Calculation.cs

---
+++ report/day2/Calculation.cs
@@ -0,0 +1,19 @@
+namespace StringPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 4;
+            int c = 3;
+
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+
+        }
+    }
+}
