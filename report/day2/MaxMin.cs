4bd69320ad4db414b84cde866f075bbbc464e1d9
iasos2 <ykkminji@gmail.com>
2024-06-26 22:44:17 +0900

    Create MaxMin.cs

---
+++ report/day2/MaxMin.cs
@@ -0,0 +1,16 @@
+namespace StringPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MaxValue;
+            Console.WriteLine(max);
+
+            int min = int.MinValue;
+            Console.WriteLine(min);
+
+            //Console.WriteLine(max + 2); //Bug Code
+        }
+    }
+}
