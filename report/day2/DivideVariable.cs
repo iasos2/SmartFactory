53ddef790465465f7854d5d9cb2edce78d7ee6a1
iasos2 <ykkminji@gmail.com>
2024-06-26 20:32:16 +0900

    Create DivideVariable.cs

---
+++ report/day2/DivideVariable.cs
@@ -0,0 +1,14 @@
+namespace StringPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 18;
+            int b = 6;
+            double c = (double)a / (double)b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
