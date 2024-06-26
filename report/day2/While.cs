92c847c628e17fc4d1806c8f4834d671a8c0a0a7
iasos2 <ykkminji@gmail.com>
2024-06-26 23:03:44 +0900

    Create While.cs

---
+++ report/day2/While.cs
@@ -0,0 +1,17 @@
+namespace StringPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i < 100)
+            {
+                Console.Write($"{i} ");
+
+                i++;
+            }
+        }
+    }
+}
