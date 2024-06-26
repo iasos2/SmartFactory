97481cf2ba652f66cf28451062d31881606b227e
iasos2 <ykkminji@gmail.com>
2024-06-26 22:43:19 +0900

    Create Double&Float_Diff.cs

---
+++ report/day2/Double&Float_Diff.cs
@@ -0,0 +1,12 @@
+namespace StringPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.141592;
+
+            float pi2 = 3.141592F;
+        }
+    }
+}
