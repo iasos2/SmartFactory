286adc2b38caa9da74f66739459a67bf1d4fcdaa
iasos2 <ykkminji@gmail.com>
2024-06-26 22:59:34 +0900

    Create For.cs

---
+++ report/day2/For.cs
@@ -0,0 +1,16 @@
+namespace StringPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. 초기화 조건 2. 종료조건
+            for (int i = 0; i <= 7; i++)
+            {
+                Console.WriteLine("Hello, World!");
+
+            }
+
+        }
+    }
+}
