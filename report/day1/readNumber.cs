3c47cc8d3be8c6c340eeb0c6f6a5d8e51beb5486
iasos2 <ykkminji@gmail.com>
2024-06-26 20:04:33 +0900

    Create readNumber.cs

---
+++ report/day1/readNumber.cs
@@ -0,0 +1,12 @@
+namespace StringPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력해 주세요 : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+        }
+    }
+}
