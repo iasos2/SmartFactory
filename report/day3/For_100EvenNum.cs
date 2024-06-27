7d377d32c0758061dff7b917df104c72b58ffc2b
iasos2 <ykkminji@gmail.com>
2024-06-27 11:34:36 +0900

    Create For_100EvenNum.cs

---
+++ report/day3/For_100EvenNum.cs
@@ -0,0 +1,17 @@
+namespace StringPrint03_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                for (int i = 100; i > 0; i -= 2)
+                {
+                    Console.WriteLine($"{i} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
