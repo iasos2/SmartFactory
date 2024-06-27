9b9bd426eb66794a498bf34e3eed400e66f3c6c8
iasos2 <ykkminji@gmail.com>
2024-06-28 08:56:25 +0900

    Create 50.cs

---
+++ report/day3/50.cs
@@ -0,0 +1,14 @@
+namespace StringPrint03-9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 50;
+            int result;
+            result = ++n;
+
+            Console.WriteLine(result);
+        }
+    }
+}
