d9e8e58ea01a5bbe6655180f8ea006aee294e9fd
iasos2 <ykkminji@gmail.com>
2024-06-28 16:56:54 +0900

    Create Star4.cs

---
+++ report/day4/Star4.cs
@@ -0,0 +1,22 @@
+namespace StringPrint04_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for(int j = 5; j>i; j--)
+                {
+                    Console.Write("*");
+                }                     
+                Console.WriteLine();
+            }
+
+        }
+    }
+} //*****
+  //****
+  //***
+  //**
+  //*
