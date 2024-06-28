8154fb8e112107bcccebc77d3bd32172db26c661
iasos2 <ykkminji@gmail.com>
2024-06-28 09:00:25 +0900

    Create Gugudan2.cs

---
+++ report/day3/Gugudan2.cs
@@ -0,0 +1,18 @@
+namespace StringPrint03_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 9; i > 1; i--)
+            {
+                for (int j = 9; j > 0; j--)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+
+                }
+            }
+
+        }
+    }
+}
