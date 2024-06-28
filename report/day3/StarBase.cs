e1e4a378c3dadfdbe5164918bfd0fd90ed4eb0dc
iasos2 <ykkminji@gmail.com>
2024-06-28 09:01:14 +0900

    Create StarBase.cs

---
+++ report/day3/StarBase.cs
@@ -0,0 +1,18 @@
+namespace StringPrint03_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
