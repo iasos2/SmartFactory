33be39a6c5232c6599cecea5ad9dcf8bac8e6831
iasos2 <ykkminji@gmail.com>
2024-07-02 11:11:58 +0900

    Create DoWhile.cs

---
+++ report/day6/DoWhile.cs
@@ -0,0 +1,23 @@
+namespace DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+
+            //while(i < 5)
+            //{
+            //    Console.WriteLine("Hello World");
+            //    i++;
+            //}
+
+            int k = 0;
+            do
+            {
+                Console.WriteLine("Hello World");
+                k++;
+            } while(k < 3);
+        }
+    }
+}
