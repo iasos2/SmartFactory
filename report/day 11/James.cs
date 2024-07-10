fd17f24c010fdb255922985083d6f6439082e063
iasos2 <ykkminji@gmail.com>
2024-07-10 09:14:47 +0900

    Create James.cs

---
+++ report/day 11/James.cs
@@ -0,0 +1,19 @@
+
+namespace StringPrint11_6
+{
+    class Program
+    {
+        static string name = "James"; //지역변수
+        //멤버변수
+        static void ShowName()
+        {
+            Console.WriteLine(name);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(name);
+
+        }
+    }
+}
