5ca79377ed159a05535d21244d6ee864ea1a3048
iasos2 <ykkminji@gmail.com>
2024-07-10 16:06:59 +0900

    Create Days.cs

---
+++ report/day12/Days.cs
@@ -0,0 +1,17 @@
+namespace StringPrint12_11
+{
+    enum Days { Sun , Mon, Tue, Wed, Thu, Fri, Sat }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)Days.Sun);
+            Console.WriteLine((int)Days.Mon);
+            Console.WriteLine((int)Days.Tue);
+            Console.WriteLine((int)Days.Wed);
+            Console.WriteLine((int)Days.Thu);
+            Console.WriteLine((int)Days.Fri);
+            Console.WriteLine((int)Days.Sat);
+        }
+    }
+}
