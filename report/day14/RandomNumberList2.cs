b188965076b1d3797983ea2a613d2a58e2216e8c
iasos2 <ykkminji@gmail.com>
2024-07-15 10:22:24 +0900

    Create RandomNumberList2.cs

---
+++ report/day14/RandomNumberList2.cs
@@ -0,0 +1,40 @@
+
+namespace StringPrint14_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int>();
+            int num = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                Random random = new Random();
+                num = random.Next(1, 100);
+                numberList.Add(num);
+            }
+            numberList.Sort();
+            numberList.Reverse();
+            foreach (int i in numberList)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            numberList.Insert(0, -7);
+            numberList.Insert(8, -100);
+            foreach (int i in numberList)
+            {
+                Console.WriteLine(i);
+            }
+            numberList.Remove(-7);
+            Console.WriteLine();
+
+            foreach (int i in numberList)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
