6e47d4fad405c0ff0e4d3660f994e3e667e58e3e
iasos2 <ykkminji@gmail.com>
2024-07-17 09:05:10 +0900

    Create StudentList.cs

---
+++ report/day15/StudentList.cs
@@ -0,0 +1,32 @@
+namespace StringPrint15_2
+{
+    class Student
+    {
+        public String Name { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.리스트 studentList를 만드세요
+            List<Student> studentList = new List<Student>();
+            //2.학생 3명을 만들어서 리스트에 넣으세요
+            Student st1 = new Student();
+            Student st2 = new Student();
+            Student st3 = new Student();
+            //3.학생은 이순신 강감찬 을지문덕
+            st1.Name = "이순신";
+            st2.Name = "강감찬";
+            st3.Name = "을지문덕";
+            //4.리스트에 학생을 담아요
+            studentList.Add(st1);
+            studentList.Add(st2);
+            studentList.Add(st3);
+            //5. 출력은 이름만 출력하고 순환문은 foreach를 사용해주세요.
+            foreach (Student student in studentList)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+    }
+}
