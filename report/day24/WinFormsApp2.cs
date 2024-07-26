45d8b4409b01bb52d3e7c62159822a4926029efc
iasos2 <ykkminji@gmail.com>
2024-07-26 16:58:58 +0900

    Create WinFormsApp2.cs

---
+++ report/day24/WinFormsApp2.cs
@@ -0,0 +1,31 @@
+//현재시간 흐르는 타이머 
+namespace WinFormsApp24_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("hh : mm : ss");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
